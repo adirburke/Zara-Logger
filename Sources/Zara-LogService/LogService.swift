6703292c7e524661178ca321811b4546e336bfae
Adir Burke @ Work <zaraserver@dev.local>
2019-10-14 16:12:49 +1100

    Test

--- Sources/Zara-LogService/LogService.swift
+++ Sources/Zara-LogService/LogService.swift
@@ -11,6 +11,8 @@ import projectConstants
 ///The log class containing all the needed methods
 open class LogService {
     
+    public static var test : Int
+    
     public init(name : String) {
         self.name = name
         start()
