057291eaef6d1dbcb83e246eed5513f8bce99cbe
Adir Burke on Laptop <adir@hanave.com>
2020-12-17 10:02:41 +1100

    Updating to add depenancies on Linux

--- Package.swift
+++ Package.swift
@@ -1,7 +1,36 @@
 // swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
+import Foundation
 
+#if os(Linux)
+let isLinux = true
+#else
+let isLinux = false
+#endif
+if isLinux {
+let package = Package(
+    name: "Zara-Logger",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Zara-Logger",
+            targets: ["Zara-Logger"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/HanavePtyLtd/projectConstants.git"),
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Zara-Logger",
+            dependencies: ["Common"]),
+    ]
+)
+} else {
 let package = Package(
     name: "Zara-Logger",
     platforms: [
@@ -27,3 +56,4 @@ let package = Package(
             dependencies: ["Common"]),
     ]
 )
+}
